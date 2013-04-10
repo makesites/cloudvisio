f18555706d3857f2ff221f8d8d8e7949b2eddcb2
Makis Tracend <makis.tracend@gmail.com>
2013-04-09 18:18:08 -0700

    adding build script

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = require('./lib/main');
+var globals = ["document", "window", "d3"],
+    globalValues = {};
+
+globals.forEach(function(g) {
+  if (g in global) globalValues[g] = global[g];
+});
+
+require("./globals");
+require("./build/cloudvisio");
+
+module.exports = cloudvisio;
+
+globals.forEach(function(g) {
+  if (g in globalValues) global[g] = globalValues[g];
+  else delete global[g];
+});
