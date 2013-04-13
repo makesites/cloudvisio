3a9958e63405f12ce81bc197a7085b388746b1b3
Makis Tracend <makis.tracend@gmail.com>
2013-04-12 18:05:41 -0700

    version 0.2.0

--- build/cloudvisio.js
+++ build/cloudvisio.js
@@ -1,107 +1,124 @@
-if( !window.cloudvisio ) (function( d3 ){
+if( !window.Cloudvisio ) (function( d3 ){
 var defaults = {
-	
+	el: "#vis", 
+	view: "graph"
 };
 // dependencies
 //var d3 = require("d3");
 
-var Cloudvisio = function( options ){
-	// fallbacks
+Cloudvisio = function( options ){
+	// variables
 	options = options || {};
-	// defaults
-	options.el = options.el || "#vis";
-	options.view = options.view || "graph";
 	
-	// initialize with options
+	// merge with defaults
+	options.el = options.el || defaults.el;
+	options.view = options.view || defaults.view;
+	
+	// save for later...
 	this.options = options;
-	
 };
 
 Cloudvisio.prototype = {
-	// this is where the axis will be stored
-	models: [], 
 	
-	// load a dataset
-	data: function( raw ){
-		// return the existing data if none is passed...
-		if (!arguments.length) return this._data;
-		// do some calculations 
-		this._data = raw;
-		// allow method chaining
-		return this;
-	}, 
+	constructor: Cloudvisio, 
 	
-	// load a dataset
-	set: function( data ){
-		data = data || false;
-		if(!data) return;
-		// check if it's and array of objects
-		//if(data instanceof Array){
-		if(data.length != "undefined"){
-			for( var i in data ){
-				this.models.push( data[i] );
-			}
-		} else {
-			// assume one element
-			this.models.push( data );
-		}
-		// allow method chaining
-		return this;
-	}, 
-	
-	// filter the data based on a regular expression
-	search: function( query, field ){
-		var exp = new RegExp(query);
-		//...
-		// allow method chaining
-		return this;
-	}, 
-	
-	match: function( query, field ){
-		var exp = new RegExp(query);
-		//...
-		// allow method chaining
-		return this;
-	}, 
-	
-	compile: require("./compile"), 
-	
-	// convert the regular expression into a string
-	verbalize: function(){
-		
+	description : function() {
+		return "Cloudvisio running on D3";
 	}
+
+};
+
+
+
+// this is where the axis will be stored
+Cloudvisio.prototype.models = [];
+
+// load a dataset
+Cloudvisio.prototype.data = function( raw ){
+	// return the existing data if none is passed...
+	if (!arguments.length) return this._data;
+	// do some calculations 
+	this._data = raw;
+	// allow method chaining
+	return this;
+};
+
+// load a dataset
+Cloudvisio.prototype.set = function( data ){
+	data = data || false;
+	if(!data) return;
+	// check if it's and array of objects
+	//if(data instanceof Array){
+	if(data.length != "undefined"){
+		for( var i in data ){
+			this.models.push( data[i] );
+		}
+	} else {
+		// assume one element
+		this.models.push( data );
+	}
+	// allow method chaining
+	return this;
+}; 
+
+// filter the data based on a regular expression
+Cloudvisio.prototype.search = function( query, field ){
+	var exp = new RegExp(query);
+	//...
+	// allow method chaining
+	return this;
+};
+
+// Internal 
+// - raw data container
+Cloudvisio.prototype._data = {};
+
+
+
+Cloudvisio.prototype.process = function( key, value ) {
+	console.log(key + " : "+value);
+};
+
+Cloudvisio.prototype.match = function( query, field ){
+	var exp = new RegExp(query);
+	//...
+	// allow method chaining
+	return this;
+};
+
+
+// convert the regular expression into a string
+Cloudvisio.prototype.verbalize = function(){
+	
+};
+
+// Helpers
+
+
+Cloudvisio.prototype.compile = function(){
 	
 };
 
 
-// visual methods
-description = function() {
-	return "cloudvisio running on d3";
-};
-
-process = function(key,value) {
-	console.log(key + " : "+value);
+// Helpers
+// - load template
+function loadTemplate(file) {
+	return file;
 }
 
 // Usage: 
 //traverse(options.data,process);
-traverse = function(o,func) {
-	for (i in o) {
-		func.apply(this,[i,o[i]]);  
-		if (typeof(o[i])=="object") {
+function traverse(o,func) {
+	for ( var i in o) {
+		func.apply(this, [i, o[i]]);  
+		if (typeof o[i] == "object" ) {
 			//going on step down in the object tree!!
-			traverse(o[i],func);
+			traverse(o[i], func);
 		}
 	}
 }
 
-// Helpers
-// - load template
-loadTemplate = function(file) {
-	return file;
-};
-	
-	
-	
+
 //module.exports = Cloudvisio;
+
 })( window.d3 );
