9f1b4dee6122e6eab47d65545ef2a3ead60a612e
Makis Tracend <makis.tracend@gmail.com>
2013-04-18 04:57:14 -0700

    loading relevant layout on init

--- build/cloudvisio.js
+++ build/cloudvisio.js
@@ -1,7 +1,7 @@
 if( !window.Cloudvisio ) (function( d3 ){
 var defaults = {
 	el: "#vis", 
-	view: "graph", 
+	layout: "stack", 
 	container: "svg", 
 	width: "100%", 
 	height: "100%"
@@ -15,16 +15,18 @@ Cloudvisio = function( options ){
 	
 	// merge with defaults
 	this.el = options.el || defaults.el;
-	options.view = options.view || defaults.view;
+	options.layout = ( options.layout || defaults.layout ).toLowerCase();
 	options.container = options.container || defaults.container;
 	options.width = options.width || defaults.width;
 	options.height = options.height || defaults.height;
 	// save options for later...
 	this.options = options;
+	// add the appropriate chart
+	this.add( this["_"+ options.layout] );
+	this.layout = this[ options.layout ] || d3.layout[ options.layout ] || false;
 	
 	// setup 
 	this._container();
-	
 };
 
 Cloudvisio.prototype = {
@@ -38,6 +40,106 @@ Cloudvisio.prototype = {
 };
 
 
+
+// adding a new (custom chart)
+Cloudvisio.prototype.add = function( chart ){
+	// evaluate the function first? 
+	this.chart = chart;
+};
+
+
+// default chart
+Cloudvisio.prototype._stack = function() {
+
+	var w = 960,
+	h = 500, 
+	label = "country", 
+	value = "population";
+	
+	var labels = this.models.map(function( data, i ){
+		return data[label];
+	});
+	
+	var values = this.models.map(function( data, i ){
+		return { x : i, y : data[value] };
+	});
+	
+	// in a stacked bar there's more than one passes from the models
+	var remapped = [values];
+	
+	var svg = d3.select( this.el + " "+ this.options.container)
+            .append("svg:g")
+            .attr("transform", "translate(10,470)");
+			
+	x = d3.scale.ordinal().rangeRoundBands([0, w-50]);
+	y = d3.scale.linear().range([0, h-50]);
+	z = d3.scale.ordinal().range(["darkblue", "blue", "lightblue"]);
+	
+	var data = d3.layout.stack()( remapped );
+	
+	x.domain(data[0].map(function(d) { return d.x; }));
+	y.domain([0, d3.max(data[data.length - 1], function(d) { return d.y0 + d.y; })]);
+	
+	// Add a group for each column.
+	var valgroup = svg.selectAll("g.valgroup")
+	.data( data )
+	.enter().append("svg:g")
+	.attr("class", "valgroup")
+	.style("fill", function(d, i) { return z(i); })
+	.style("stroke", function(d, i) { return d3.rgb(z(i)).darker(); });
+	
+	// Add a rect for each date.
+	var rect = valgroup.selectAll("rect")
+	.data(function(d){return d;})
+	.enter().append("svg:rect")
+	.attr("x", function(d) { return x(d.x); })
+	.attr("y", function(d) { return -y(d.y0) - y(d.y); })
+	.attr("height", function(d) { return y(d.y); })
+	.attr("width", x.rangeBand());
+	
+};
+
+Cloudvisio.prototype._pie = function() {
+
+ 
+var m = 0,
+    r = 100,
+	ir = 0,
+    z = d3.scale.category20c(), 
+	label = "country", 
+	value = "population";
+ 
+ 
+	var labels = this.models.map(function( data, i ){
+		return data[label];
+	});
+	
+	var values = this.models.map(function( data, i ){
+		return data[value];
+	});
+	
+	var data = [values];
+	
+// Insert an svg:svg element (with margin) for each row in our dataset. A
+// child svg:g element translates the origin to the pie center.
+var svg = d3.select( this.el + " "+ this.options.container)
+    .data( data )
+  .append("svg:g")
+    .attr("transform", "translate(" + (r + m) + "," + (r + m) + ")");
+ 
+// The data for each svg:svg element is a row of numbers (an array). We pass
+// that to d3.layout.pie to compute the angles for each arc. These start and end
+// angles are passed to d3.svg.arc to draw arcs! Note that the arc radius is
+// specified on the arc, not the layout.
+svg.selectAll("path")
+    .data(d3.layout.pie())
+  .enter().append("svg:path")
+    .attr("d", d3.svg.arc()
+    .innerRadius( ir )
+    .outerRadius(r))
+    .style("fill", function(d, i) { return z(i); });
+ 
+};
 
 // this is where the axis will be stored
 Cloudvisio.prototype.models = [];
@@ -107,13 +209,13 @@ Cloudvisio.prototype.verbalize = function(){
 // rendering the visualization (generated once)
 Cloudvisio.prototype.render = function(){
 	d3.select( this.el + " "+ this.options.container).data(this.models);
-	//.transition().duration(500).call(chart);
+	this.chart();
+	//.transition().duration(500).call( this.chart() );
 };
 
 // updating visualization (on every tick)
 Cloudvisio.prototype.update = function(){
-	d3.select( this.el + " "+ this.options.container).data(this.models);
-	//.transition().duration(500).call(chart);
+	
 };
 
 // Internal methods
