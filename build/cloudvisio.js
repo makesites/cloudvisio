ff79fc5d682b4bd633c1db79b391e82462389524
Makis Tracend <makis.tracend@gmail.com>
2013-04-20 20:01:48 -0700

    updating data manipulation methods: axis(), find(), group(), keys()

--- build/cloudvisio.js
+++ build/cloudvisio.js
@@ -88,6 +88,77 @@ Cloudvisio.prototype.search = function( query, field ){
 	return this;
 };
 
+// return the matchs of a regular expression
+Cloudvisio.prototype.find = function( query, string ){
+	if( query instanceof Array) query = query.join("|");
+	var regexp = new RegExp(query, "gi");
+	return string.match(regexp);
+};
+
+// retrieve all keys from a data block (currently non-recursive)
+Cloudvisio.prototype.keys = function( data ){
+	// fallback to the raw data
+	data = data || this.data();
+	//
+	var keys = [];
+	for(var i in data){
+		var attr = Object.keys(  data[i] );
+		for( var j in attr ){
+			// don't add the same key 
+			if( keys.indexOf( attr[j] ) > -1 ) continue;
+			keys.push( attr[j] );
+		}
+	}
+	//
+	return keys;
+};
+
+// set an axis key (or return the whole array
+Cloudvisio.prototype.axis = function( key ){
+	// return the existing data if none is passed...
+	if (!arguments.length) return Object.keys( this.models[0] );
+	// focus on a specific subset?
+	var data = this.data();
+	// lookup the key in the raw data
+	for(var i in data){
+		// create model if necessary
+		if(typeof this.models[i] == "undefined" ) this.models[i] = {};
+			
+		// there's currenty a 1-1 match between the data length and the models length...
+		if( typeof data[i][key] == "string" ){
+			this.models[i][key] = data[i][key];
+		} else if( typeof data[i][key] == "object" ){
+			// by priority look up a desciptive key in the object
+			this.models[i][key] = data[i][key].value || data[i][key].name || data[i][key].title || data[i][key].id || data[i][key];
+		} else {
+			// not supporting any other types for now...
+			this.models[i][key] = null;
+		}
+		//
+		
+	}
+	// maintain chanability
+	return this;
+};
+
+
+// group models based on axis results
+Cloudvisio.prototype.group = function( groups, axis){ 
+	// create the regular expression
+	// lookup the right axis
+	for( var i in this.models ){
+		var model = this.models[i];
+		if( typeof model[axis] != "undefined"){
+			var matches = this.find(groups, model[axis]);
+			// process results
+			model["group_"+axis] = matches;
+		} else {
+			model["group_"+axis] = null;
+		}
+	}
+	//'match(/(high)/gi, ).;
+}; 
+
 // Internal 
 // - raw data container
 Cloudvisio.prototype._data = {};
@@ -161,7 +232,7 @@ Cloudvisio.prototype.charts.stack = function() {
 	
 	var svg = d3.select( this.el + " "+ this.options.container)
             .append("svg:g")
-            .attr("transform", "translate(10,470)");
+            .attr("transform", "translate(30,470)");
 			
 	x = d3.scale.ordinal().rangeRoundBands([0, w]);
 	y = d3.scale.linear().range([0, h]);
@@ -188,13 +259,46 @@ Cloudvisio.prototype.charts.stack = function() {
 	.attr("height", function(d) { return y(d.y); })
 	.attr("width", x.rangeBand());
 	
+	//parse = d3.time.format("%m/%Y").parse,
+    //format = d3.time.format("%b");
+	
+	// Add a label per date.
+	svg.selectAll("text")
+		.data( labels )
+		.enter().append("svg:text")
+		.attr("x", function(d) { return x(d) + x.rangeBand() / 2; })
+		.attr("y", 6)
+		.attr("text-anchor", "middle")
+		.attr("dy", ".71em")
+		.text(function(d) { return d; });
+	
+	// Add y-axis rules.
+	var rule = svg.selectAll("g.rule")
+		.data(y.ticks(5))
+		.enter().append("svg:g")
+		.attr("class", "rule")
+		.attr("transform", function(d) { return "translate(0," + -y(d) + ")"; });
+		
+	rule.append("svg:line")
+		.attr("x2", w)
+		.style("stroke", function(d) { return d ? "#fff" : "#000"; })
+		.style("stroke-opacity", function(d) { return d ? 0.3 : null; });
+		
+	rule.append("svg:text")
+		.attr("x", -20)
+		.attr("dy", ".35em")
+		.text(d3.format(",d"));
+		
+
 };
+
+var arc = d3.svg.arc();
 
 Cloudvisio.prototype.charts.pie = function() {
 
 	var self = this;
 	
-	var r = 100,
+	var r = 300, // width/2
 		ir = 0,
 		label = "country", 
 		value = "population";
@@ -224,12 +328,48 @@ Cloudvisio.prototype.charts.pie = function() {
 	svg.selectAll("path")
 		.data(d3.layout.pie())
 		.enter().append("svg:path")
-		.attr("d", d3.svg.arc()
-		.innerRadius( ir )
-		.outerRadius(r))
+		.attr("class", "slice")    //allow us to style things in the slices (like text)
+		.attr("d", arc
+			.innerRadius( ir )
+			.outerRadius(r))
 		.style("fill", function(d, i) { return self.color( i ); });
+
+	// animate slices
+	svg.selectAll("path").transition()
+		.ease("cubic")
+		.duration(2000)
+		.attrTween("d", tweenPie);
+		
+	// labels
+	/*
+	svg.append("svg:g")
+		.attr("class", "label_group")
+		//.attr("transform", "translate(" + (w/2) + "," + (h/2) + ")")
+		.append("svg:text") 
+		.style("font-size", function(d) { return (d.value * 1.5); })                                   //add a label to each slice
+		.attr("fill", "grey")
+		.attr("opacity", "100")
+		.attr("transform", function(d) {                    //set the label's origin to the center of the arc
+			//we have to make sure to set these before calling arc.centroid
+			d.innerRadius = 0;
+			d.outerRadius = r;
+			return "translate(" + arc.centroid(d) + ")";        //this gives us a pair of coordinates like [50, 50]
+		})
+		.attr("text-anchor", "middle")                          //center the text on it's origin
+		.text(function(d, i) { return labels[i]; });        //get the label from our original data array
+		.transition().ease("cubic").duration(2000).attrTween("transform", textTween);
+	*/
 	
 };
+
+function tweenPie(b) {
+	b.innerRadius = 0;
+	var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
+	return function(t) {
+		return arc(i(t));
+	};
+} 
+
 
 // rendering the visualization (generated once)
 Cloudvisio.prototype.render = function(){
