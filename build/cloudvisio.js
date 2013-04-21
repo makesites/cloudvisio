9a3c291a38aae62b317ccb2890be85c3643aa3de
Makis Tracend <makis.tracend@gmail.com>
2013-04-21 04:18:00 -0700

    starting to record chart settings in the options

--- build/cloudvisio.js
+++ build/cloudvisio.js
@@ -8,9 +8,9 @@ var defaults = {
 	// The color scale will be assigned by index, but if you define your data using objects, you could pass
 	// in a named field from the data object instead, such as `d.name`. Colors are assigned lazily, 
 	// so if you want deterministic behavior, define a domain for the color scale.
-	colors: d3.scale.category20c()
+	colors: d3.scale.category20c(), 
 	//colors: d3.scale.ordinal().range(["darkblue", "blue", "lightblue"]);
-	
+	chart: {}
 };
 
 // dependencies
@@ -145,18 +145,29 @@ Cloudvisio.prototype.axis = function( key ){
 
 // group models based on axis results
 Cloudvisio.prototype.group = function( groups, axis){ 
-	// create the regular expression
+	// convert groups to lower case
+	groups = groups.join("`").toLowerCase().split("`");
 	// lookup the right axis
 	for( var i in this.models ){
 		var model = this.models[i];
 		if( typeof model[axis] != "undefined"){
 			var matches = this.find(groups, model[axis]);
 			// process results
-			model["group_"+axis] = matches;
+			//if(matches !== null);
+			if(matches instanceof Array){
+				// are we expecting more than one matches??
+				var group = matches.pop().toLowerCase();
+				model["group_"+axis] = groups.indexOf( group);
+			} else {
+				model["group_"+axis] = -1;
+			}
 		} else {
-			model["group_"+axis] = null;
+			//model["group_"+axis] = null;
+			model["group_"+axis] = -1;
 		}
 	}
+	// save groups for later
+	this.options.chart["group_"+axis] = groups;
 	//'match(/(high)/gi, ).;
 }; 
 
@@ -373,14 +384,115 @@ function tweenPie(b) {
 	};
 } 
 
+// force chart
+Cloudvisio.prototype.charts.force = function() {
+
+	var self = this;
+	var svg = d3.select( this.el + " "+ this.options.container);
+	var width = 1000,
+    height = 1000;
+
+	var nodes = models();
+	
+	var force = d3.layout.force()
+		.charge(-20)
+		.linkDistance(30)
+		.size([width, height])
+		.nodes( nodes )
+		.links([])
+		.start();
+
+	/*
+	var groups = d3.nest().key(function(d) { return d.group & 3; }).entries( nodes );
+	
+	var groupPath = function(d) {
+		return "M" + d3.geom.hull(d.values.map(function(i) { return [i.x, i.y]; })).join("L") + "Z";
+	};
+	
+	var groupFill = function(d, i) { return self.color(i & 3); };
+	*/
+	
+	var node = svg.selectAll(".node")
+		.data( nodes ).enter()
+		.append("circle")
+		.attr("class", "node")
+		.attr("r", 5)
+		.style("fill", function(d) { return self.color( d.group ); })
+		.call(force.drag);
+
+	node.append("title")
+		.text(function(d) { return d.name; });
+	
+	force.on("tick", function(e) {
+	
+		
+		// Push different nodes in different directions for clustering.
+		var k = 6 * e.alpha;
+		nodes.forEach(function(o, i) {
+			//o.x += i & 2 ? k : -k;
+			//o.y += i & 1 ? k : -k;
+			o.x += (o.group + 2) & 2 ? k : -k;
+			o.y += (o.group + 2) & 1 ? k : -k;
+		});
+		
+		node.attr("cx", function(d) { return d.x; })
+			.attr("cy", function(d) { return d.y; });
+			
+	/*
+	svg.selectAll("path")
+		.data(groups)
+		.attr("d", groupPath)
+		.enter().insert("path", "circle")
+		.style("fill", groupFill)
+		.style("stroke", groupFill)
+		.style("stroke-width", 40)
+		.style("stroke-linejoin", "round")
+		.style("opacity", .2)
+		.attr("d", groupPath);
+	*/
+
+	});
+
+	// local 
+	function models(){
+		var d = self.models;
+		// get keys
+		var keys = self.keys( d );
+		// required data: 
+		// - name (string)
+		var name, group;
+		// - group (integer)
+		if( keys.indexOf("id") > -1 ) name = "id";
+		if( keys.indexOf("title") > -1 ) name = "title";
+		if( keys.indexOf("label") > -1 ) name = "label";
+		if( keys.indexOf("name") > -1 ) name = "name";
+		for( var i in keys ){
+			if( keys[i].search(/group_/gi) === 0 ) group =  keys[i];
+		}
+		//
+		var result = [];
+		for( var j in d ){
+			result.push({
+				name : d[j][name], 
+				group : d[j][group]
+			});
+		}
+		//
+		return result;
+	}
+	
+};
+
 
 // rendering the visualization (generated once)
 Cloudvisio.prototype.render = function(){
-	d3.select( this.el + " "+ this.options.container).data(this.models);
+	
 	var chart = this.chart();
 	if( chart !== null)
 		chart.call(this, arguments);
 	//.transition().duration(500).call( this.chart() );
+	
+	
 };
 
 // updating visualization (on every tick)
@@ -403,10 +515,28 @@ Cloudvisio.prototype.set = function( obj ){
 // Internal methods
 // creates the chart container
 Cloudvisio.prototype._container = function(){
-	d3.select( this.el )
-		.append( this.options.container )
-		.attr("width", this.options.width)
-		.attr("height", this.options.height);
+	
+	var width = 1000,
+    height = 1000;
+
+	var svg = d3.select( this.el )
+		.append( this.options.container );
+	
+	function redraw() {
+		svg.attr("transform", "translate("+ d3.event.translate +")" + " scale("+ d3.event.scale +")");
+	}
+	
+	svg.attr({
+			"width": this.options.width,
+			"height":  this.options.height
+		})
+		.attr("viewBox", "0 0 " + width + " " + height )
+		.attr("preserveAspectRatio", "xMidYMid meet")
+		.attr("pointer-events", "all")
+		.call(d3.behavior.zoom().on("zoom", redraw));
+		
+	return svg;
+	
 };
 	
 
