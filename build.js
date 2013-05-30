0d83adc6a447a52a052e91839b9e718b1fcae79d
Makis Tracend <makis.tracend@gmail.com>
2013-05-30 03:50:11 -0700

    moving set() from render to options

--- build.js
+++ build.js
@@ -1,31 +1,31 @@
 // settings
 var FILE_ENCODING = 'utf-8',
-    EOL = '\n';
- 
+	EOL = '\n';
+
 // Dependencies
 var _cli = require('commander'),
-	_uglify = require("uglify-js"), 
-    _jshint = require('jshint'),
-	_handlebars = require('hbs'), 
-    _fs = require('fs');
- 
- 
+	_uglify = require("uglify-js"),
+	_jshint = require('jshint'),
+	_handlebars = require('hbs'),
+	_fs = require('fs');
+
+
 // Helper, will generate a CSV if package.json contains multiple licenses
 _handlebars.registerHelper('license', function(items){
-    items = items.map(function(val){
-        return val.type;
-    });
-    return items.join(', ');
+	items = items.map(function(val){
+		return val.type;
+	});
+	return items.join(', ');
 });
- 
- 
- // Logic 
+
+
+ // Logic
  // - concatinate all files
 concat({
-    src : [
+	src : [
 		'lib/_start.js',
-		'lib/options.js',
 		'lib/constructor.js',
+		'lib/options.js',
 		'lib/data.js',
 		'lib/math.js',
 		'lib/language.js',
@@ -37,14 +37,14 @@ concat({
 		'lib/color.js',
 		'lib/utils.js',
 		'lib/_end.js'
-    ],
-    dest : 'build/cloudvisio.js'
+	],
+	dest : 'build/cloudvisio.js'
 });
 
 
 // - Validate js
 lint('build/cloudvisio.js', function(){
-    
+
 	// - Create / save minified file
 	uglify('build/cloudvisio.js', 'build/cloudvisio-min.js');
 
@@ -55,69 +55,69 @@ lint('build/cloudvisio.js', function(){
 //
 // Methods
 function concat(opts) {
-    var fileList = opts.src;
-    var distPath = opts.dest;
-	
+	var fileList = opts.src;
+	var distPath = opts.dest;
+
 	var lib = fileList.map(function(filePath){
-            return _fs.readFileSync(filePath, FILE_ENCODING);
-        });
-	
+			return _fs.readFileSync(filePath, FILE_ENCODING);
+		});
+
 	var template = _handlebars.compile( lib.join(EOL) );
-	 
+
 	//reuse package.json data and add build date
 	var data = JSON.parse( _fs.readFileSync('package.json', FILE_ENCODING) );
 	data.build_date = (new Date()).toUTCString();
-	
+
 	// Save uncompressed file
-    _fs.writeFileSync(distPath, template(data), FILE_ENCODING);
-    console.log(' '+ distPath +' built.');
-	
+	_fs.writeFileSync(distPath, template(data), FILE_ENCODING);
+	console.log(' '+ distPath +' built.');
+
 }
 
 
 function uglify(srcPath, distPath) {
 	/*
-    var
-      jsp = uglyfyJS.parser,
-      pro = uglyfyJS.uglify,
-      ast = jsp.parse( _fs.readFileSync(srcPath, FILE_ENCODING) );
- 
-    ast = pro.ast_mangle(ast);
-    ast = pro.ast_squeeze(ast);
- 	*/
-	
-	var result = _uglify.minify(srcPath, { output: { 
+	var
+	  jsp = uglyfyJS.parser,
+	  pro = uglyfyJS.uglify,
+	  ast = jsp.parse( _fs.readFileSync(srcPath, FILE_ENCODING) );
+
+	ast = pro.ast_mangle(ast);
+	ast = pro.ast_squeeze(ast);
+	*/
+
+	var result = _uglify.minify(srcPath, { output: {
 		comments : /@name|@url|@license/
 	} });
-	
-    _fs.writeFileSync(distPath, result.code, FILE_ENCODING);
-    console.log(' '+ distPath +' built.');
+
+	_fs.writeFileSync(distPath, result.code, FILE_ENCODING);
+	console.log(' '+ distPath +' built.');
 }
- 
+
 function lint(path, callback) {
-    var buf = _fs.readFileSync(path, 'utf-8');
-    // remove Byte Order Mark
-    buf = buf.replace(/^\uFEFF/, '');
- 
-    _jshint.JSHINT(buf);
- 
-    var nErrors = _jshint.JSHINT.errors.length;
- 
-    if (nErrors) {
+	var buf = _fs.readFileSync(path, 'utf-8');
+	// remove Byte Order Mark
+	buf = buf.replace(/^\uFEFF/, '');
+
+	_jshint.JSHINT(buf);
+
+	var nErrors = _jshint.JSHINT.errors.length;
+
+	if (nErrors) {
 		// ruff output of errors (for now)
- 		console.log(_jshint.JSHINT.errors);
-        console.log(' Found %j lint errors on %s, do you want to continue?', nErrors, path);
-		
-        _cli.choose(['no', 'yes'], function(i){
-            if (i) {
-                process.stdin.destroy();
-                if(callback) callback();
-            } else {
-                process.exit(0);
-            }
-        });
-    } else if (callback) {
-        callback();
-    }
+		console.log(_jshint.JSHINT.errors);
+		console.log(' Found %j lint errors on %s, do you want to continue?', nErrors, path);
+
+		_cli.choose(['no', 'yes'], function(i){
+			if (i) {
+				process.stdin.destroy();
+				if(callback) callback();
+			} else {
+				process.exit(0);
+			}
+		});
+	} else if (callback) {
+		callback();
+	}
 }
 
