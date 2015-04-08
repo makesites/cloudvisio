0dc41139702137865f2e9a2160d0cae90cc96839
Makis Tracend <makis.tracend@gmail.com>
2015-04-07 23:56:42 -0700

    preserving comments

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 browserify index.js -o build/cloudvisio.js
-uglifyjs build/cloudvisio.js > build/cloudvisio-min.js
-#uglifyjs build/cloudvisio.js -m --comments '/Tick.js|Copyright/' > build/cloudvisio-min.js
+uglifyjs build/cloudvisio.js -m --comments '/@name|@url|@author|@license/' > build/cloudvisio-min.js
 
 exit;
